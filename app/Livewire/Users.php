f99c4848acffe1d5851e5e97fe6ad78b45a35f1c
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2025-06-14 12:01:34 +0100

    Wip

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -10,24 +10,32 @@ class Users extends Base
     public $sortBy = 'firstname';
     public function render()
     {
-        if ($this->search) {
-            $users = User::query()
-                ->whereHas('roles')
-                ->where('firstname', 'like', '%' . $this->search . '%')
-                ->Orwhere('email', 'like', '%' . $this->search . '%')
-                ->paginate(10);
 
-            return view(
-                'livewire.users',
-                ['users' => $users]
-            );
-        } else {
-            $users = User::whereHas('roles')->orderBy($this->sortBy, $this->sortDirection)
-                ->paginate($this->perPage);
-            return view(
-                'livewire.users',
-                ['users' => $users]
-            );
-        }
+      if ($this->search) {
+        $users = User::query()
+          ->whereHas('roles', function ($query) {
+            $query->where('name', '!=', 'patient');
+          })
+          ->where(function ($query) {
+            $query->where('firstname', 'like', '%' . $this->search . '%')
+              ->orWhere('email', 'like', '%' . $this->search . '%');
+          })
+          ->paginate(10);
+
+        return view(
+          'livewire.users',
+          ['users' => $users]
+        );
+      } else {
+        $users = User::whereHas('roles', function ($query) {
+          $query->where('name', '!=', 'patient');
+        })->orderBy($this->sortBy, $this->sortDirection)
+          ->paginate($this->perPage);
+
+        return view(
+          'livewire.users',
+          ['users' => $users]
+        );
+      }
     }
 }
