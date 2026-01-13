ee46e1ac51450b82582ef6edd041c0b5e98e6632
Mahmud Bakale <mahmudbakale@Mahmuds-MacBook-Air.local>
2026-01-13 07:51:26 +0100

    Wip

--- app/Livewire/AllLab.php
+++ app/Livewire/AllLab.php
@@ -5,19 +5,21 @@ namespace App\Livewire;
 
 use App\Models\LabRequest;
 use Livewire\Component;
+use Livewire\WithPagination;
 
-class AllLab extends Component
+class AllLab extends Base
 {
+    use WithPagination;
+
     public $patientId;
     public $locationId;
     public $categoryId;
     public $startDate;
     public $endDate;
-    public $perPage = 10;
 
     public function render()
     {
-      $labRequests = LabRequest::query()
+        $labRequests = LabRequest::query()->where('status', '!=', 'Result Ready')
             ->when($this->patientId, function ($query) {
                 $query->where('patient_id', $this->patientId);
             })
@@ -27,12 +29,19 @@ class AllLab extends Component
             ->when($this->startDate && $this->endDate, function ($query) {
                 $query->whereBetween('request_date', [$this->startDate, $this->endDate]);
             })
+            ->when($this->search, function ($query) {
+                $query->whereHas('patient.user', function($q) {
+                    $q->where('firstname', 'like', '%' . $this->search . '%')
+                      ->orWhere('lastname', 'like', '%' . $this->search . '%');
+                })->orWhereHas('test', function($q) {
+                    $q->where('name', 'like', '%' . $this->search . '%');
+                });
+            })
             ->orderByRaw("CASE 
                 WHEN status = 'Pending' THEN 1 
                 WHEN status = 'Specimen Collected' THEN 2 
-                WHEN status = 'Result Ready' THEN 3 
-                ELSE 4 END")
-            ->orderBy('created_at', 'desc')
+                ELSE 3 END")
+            ->latest()
             ->paginate($this->perPage);
 
         return view('livewire.all-lab', [
