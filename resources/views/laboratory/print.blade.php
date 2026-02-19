874f2d6caf6238a431d2d9f3ced7d80cf05f9acf
Mahmud Bakale <apps-mahmud.bakale@gmail.com>
2026-02-19 18:29:37 +0100

    files

--- resources/views/laboratory/print.blade.php
+++ resources/views/laboratory/print.blade.php
@@ -209,7 +209,7 @@
         <strong>{{ $result->user ? $result->user->FullName() : 'N/A' }}</strong>
     @endif
     <br/>
-    {{ app(App\Settings\SystemSettings::class)->website ?? 'www.sahadhospitals.com' }} - {{ app(App\Settings\SystemSettings::class)->email ?? 'info@sahadhospitals.com' }} - {{ app(App\Settings\SystemSettings::class)->phone ?? '07070111111' }}
+    {{ app(App\Settings\SystemSettings::class)->website ?? 'www.gamjipremierclinics.com.com' }} - {{ app(App\Settings\SystemSettings::class)->email ?? 'info@gamjipremierclinics.com' }} - {{ app(App\Settings\SystemSettings::class)->phone ?? '070' }}
 </footer>
 </body>
 </html>
